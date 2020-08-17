ff26a2c9b0ac51204f51b3dee99119eeabf3f252
shorn <shorn@transparency.consulting>
2020-08-18 09:53:26 +1200

    Added addChat file

---
+++ src/Pulse/Chat/addChat.js
@@ -0,0 +1,30 @@
+//updating requeststatus - index
+//updating chat, can add attribute , if not existing
+
+import updateIteminDynamoGeneric from 'AWS/updateIteminDynamoGeneric'
+
+//export default async function saveStageinDynamo(key, chatObj) {
+    export default async function addChat() {
+
+    // const {email, projectid} = key
+
+    
+    let email = "sht@der.com"
+
+    let projectid = 20081113242455
+
+    let chatObj = {34543252335 : { email : "dafdafdaf" }}
+
+   console.log(email)
+
+   console.log(projectid)
+
+   console.log(chatObj)
+
+   const attrToUpdate = "chat"
+
+    const response = await updateIteminDynamoGeneric(email,projectid,attrToUpdate, chatObj)
+
+    return response
+}
+
