49987d70611410a235e70c4ca2fee55b2dca096d
shaun jacob <sjacobnz86@gmail.com>
2020-09-12 01:33:19 +1200

    Modified projectdetail to bootstrap

--- src/Pulse/ProjectDetail/components/ProjectDetailStaffView.js
+++ src/Pulse/ProjectDetail/components/ProjectDetailStaffView.js
@@ -19,55 +19,57 @@ export default function ProjectDetailStaffView({
 
   //a filter function whose output that will be used as a parameter to map. cant use assignmnets inside map
   const checkedItemsList = checkedItems.map((item, index) => (
-    <li className="list-group-item list-group-item-info" key={index}>
-      {item}
-    </li>
+    <div class="p-3  bg-primary text-black" key={index}> {item}</div>
+    // <li className="list-group-item list-group-item-info" key={index}>
+    //   {item}
+    // </li>
   ));
 
   const txtItemsList = txtItems.map((item, index) => {
-     
-    const {label,value} = item
+
+    const { label, value } = item
 
     // console.log(label)
     // console.log(value)
-    
-    return(
-    <li className="list-group-item list-group-item-info" key={index}>
-        <div className="row">
-        <div className="col-6">{label + ": "}</div>
-        <div className="col-6">{value}</div>
-        </div>   
-    </li>
-  )
 
-}
+    return (
+      <div class="p-3  bg-primary text-black" key={index}>
+        <div className="row">
+          <div className="col-6">{`${label} :`}</div>
+          <div className="col-6">{value}</div>
+        </div>
+      </div>
+
+    )
+
+  }
   );
 
-//   console.log(txtItems)
+  //   console.log(txtItems)
 
 
 
   return (
-<React.Fragment>
-      <ul className="list-group">
-      <div class="p-3 mb-1 bg-primary text-black">{rowlabels.initialDate + " : "+ getDateStringFromID(projectid.toString())}</div>
+    <React.Fragment>
+
+      <div class="p-3 mb-1 bg-primary text-black">{rowlabels.initialDate + " : " + getDateStringFromID(projectid.toString())}</div>
       <div class="p-3 mb-1 bg-primary text-black">{rowlabels.id + " : " + projectid}</div>
       <div class="p-3 mb-1 bg-primary text-black">{rowlabels.email + " : " + email}</div>
       <div class="p-3 mb-1 bg-primary text-white">{rowlabels.status + " : " + status}</div>
       <div class="p-3 mb-1 bg-primary text-black">{rowlabels.optionsLabel}</div>
-      
+
       {/* <li className="list-group-item list-group-item-primary">{rowlabels.id + " : " + projectid}</li>
       <li className="list-group-item list-group-item-primary">{rowlabels.email + " : " + email}</li>
       <li className="list-group-item list-group-item-secondary">{rowlabels.status + " : " + status}</li>
       <li className="list-group-item list-group-item-primary">{rowlabels.optionsLabel}</li> */}
-     
+
       {checkedItemsList}
-      {txtItemsList }
-      </ul>
+      {txtItemsList}
+
 
       <DisplayFiles files={files} />
 
-      </React.Fragment>
-     
+    </React.Fragment>
+
   );
 }
